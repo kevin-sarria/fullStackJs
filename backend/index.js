54d23337d6e6063a97e7f0d82c9a3a6866fa3d0c
Kevin Sarria <reysabueso256@gmail.com>
2022-07-10 22:58:03 -0500

    Avances curso js moderno Proyecto MERN

---
+++ backend/index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import conectarDB from './config/db.js';
+import dotenv from 'dotenv';
+import veterinarioRoutes from './routes/veterinarioRoutes.js';
+
+
+
+const app = express();
+
+dotenv.config();
+
+conectarDB();
+
+app.use('/api/veterinarios', veterinarioRoutes);
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor funcionando en el puerto ${PORT}`);
+});
